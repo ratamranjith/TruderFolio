c86bfd0b4f43cbce3af9bd86e4e31291bf074ae5
RANJITH KUMAR VENKATACHALAM <ratamranjith@gmail.com>
2024-04-15 13:01:57 +0530

    Update dbConfig.js

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,1 +1,15 @@
+// Database Connection
+const mongoose = require("mongoose");
+console.log(process.env.mongo_url);
 
+mongoose.connect(process.env.mongo_url);
+const connection = mongoose.connection;
+connection.on("error", () => {
+  console.log("Error Connecting to Database");
+  process.exit(1);
+});
+connection.on("connected", () => {
+  console.log("Connecting to Database Successful");
+});
+
+module.exports = connection;
